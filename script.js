ad7c59e46d09b469bc83ddd788cd88c642beecec
Asam Shan <asamshan456@gmail.com>
2018-07-21 16:54:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button")
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function setRandColor() {
+	color1.value = "#"+(Math.random()*0xFFFFFF<<0).toString(16); 
+	color2.value = "#"+(Math.random()*0xFFFFFF<<0).toString(16);
+	setGradient(); 
+}
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", setRandColor)
+
+
